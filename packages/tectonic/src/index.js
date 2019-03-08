3bcd9cf32a9678be4783bd574186753a396c65ab
Benjamin Halsted <bhalsted@guidespark.com>
2019-03-08 07:36:35 -0800

    v3.4.1

--- packages/tectonic/src/index.js
+++ packages/tectonic/src/index.js
@@ -3,9 +3,12 @@
 export default from './decorator';
 export Model from './model';
 export Manager from './manager';
+export Query from './query';
+
 // Export the <Loader /> wrapper component
 export Loader from './component';
 export reducer from './reducer';
+
 // Resolvers
 export BaseResolver from './resolver/baseResolver.js';
 export Status from './status/status.js';
